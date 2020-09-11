5ae19149042a8d01be43fc0dfb6923a46c0b1cef
MikhailChekal <71117994+MikhailChekal@users.noreply.github.com>
2020-09-11 10:57:19 +0300

    Add files via upload

--- 7.py
+++ 7.py
@@ -1,7 +1,13 @@
+import math
 import turtle
+turtle.tracer(0)
 
 turtle.shape('turtle')
 turtle.speed(100)
-for i in range(1000):
-    turtle.forward(0.2 + 0.1 * i)
-    turtle.left(10)
+delta_phi = 5 / (2 * math.pi)
+for i in range(1, 10000):
+    turtle.left(delta_phi)
+    turtle.forward(0.001 * delta_phi * math.sqrt(1 + (delta_phi * i) ** 2))
+
+turtle.tracer(1)
+turtle.mainloop()
